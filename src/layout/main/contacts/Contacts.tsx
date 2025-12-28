a747f7fe859f29b1d21e4ed6f4a80de496cc1cbc
ya_yarek <kvachonoky@gmail.com>
2025-12-28 22:18:31 +0300

    28_12

--- src/layout/main/contacts/Contacts.tsx
+++ src/layout/main/contacts/Contacts.tsx
@@ -3,17 +3,42 @@ import {Container} from "../../../components/container/Container.ts";
 import {FlexWrapper} from "../../../components/flexWrapper/FlexWrapper.tsx";
 import {S} from "./Contacts_Style.ts"
 import {Fade} from "react-awesome-reveal";
+import emailjs from '@emailjs/browser';
+import {ElementRef, useRef} from "react";
 
 
 export const Contacts = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e:any) => {
+        e.preventDefault();
+        
+        if(!form.current) return
+
+        emailjs
+            .sendForm('service_faknwqa', 'template_j59tynz', form.current, {
+                publicKey: 'ZslYiiqbVnYMQzHyF',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.target.reset();
+    };
 
     const inputType: Array<
         {
-            name: string
+            title: string
             type: string
+            name: string
         }> = [
-        {name: "Name", type: "text"},
-        {name: "Email", type: "email"}
+        {title: "Name", type: "text", name: "user_name"},
+        {title: "Subject", type: "text", name: "subject"},
+        {title: "Email", type: "email", name: "email"}
     ]
 
     return (
@@ -22,15 +47,15 @@ export const Contacts = () => {
                 <Fade cascade={true} delay={350}>
                     <FlexWrapper direction={"column"} justify={"center"} align={"center"}>
                         <SectionTitle title={"Contact me"}/>
-                        <S.Form>
+                        <S.Form ref={form} onSubmit={sendEmail}>
                             {inputType.map((t, i) => {
                                 return (
-                                    <S.Label key={i}>{t.name}
-                                        <S.Field type={t.type}/></S.Label>
+                                    <S.Label key={i}>{t.title}
+                                        <S.Field required type={t.type} name={t.name}/></S.Label>
                                 )
                             })}
                             <S.Label> Message
-                                <S.Textarea/>
+                                <S.Textarea required name="message"/>
                             </S.Label>
                             <S.Button type="submit">Send</S.Button>
                         </S.Form>
