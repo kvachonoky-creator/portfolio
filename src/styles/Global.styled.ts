efd0a84272d9cb8b76743c299f4fca2283d72fe0
ya_yarek <kvachonoky@gmail.com>
2026-06-17 23:18:01 +0300

    fix font paths for vercel

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -13,72 +13,72 @@ export const GlobalStyle = createGlobalStyle`
 
     @font-face {
         font-family: Nunito;
-        src: url('../../public/fonts/Nunito-Regular.eot?') format("eot"),
-        url('../../public/fonts/Nunito-Regular.woff2') format("woff2"),
-        url('../../public/fonts/Nunito-Regular.ttf') format("truetype");
+        src: url('/fonts/Nunito-Regular.eot?') format("eot"),
+        url('/fonts/Nunito-Regular.woff2') format("woff2"),
+        url('/fonts/Nunito-Regular.ttf') format("truetype");
         font-weight: 400;
         font-style: normal;
     }
 
     @font-face {
         font-family: Nunito;
-        src: url('../../public/fonts/Nunito-SemiBold.eot?') format("eot"),
-        url('../../public/fonts/Nunito-SemiBold.woff2') format("woff2"),
-        url('../../public/fonts/Nunito-SemiBold.ttf') format("truetype");
+        src: url('/fonts/Nunito-SemiBold.eot?') format("eot"),
+        url('/fonts/Nunito-SemiBold.woff2') format("woff2"),
+        url('/fonts/Nunito-SemiBold.ttf') format("truetype");
         font-weight: 600;
         font-style: normal;
     }
 
     @font-face {
         font-family: Nunito;
-        src: url('../../public/fonts/Nunito-Bold.eot?') format("eot"),
-        url('../../public/fonts/Nunito-Bold.woff2') format("woff2"),
-        url('../../public/fonts/Nunito-Bold.ttf') format("truetype");
+        src: url('/fonts/Nunito-Bold.eot?') format("eot"),
+        url('/fonts/Nunito-Bold.woff2') format("woff2"),
+        url('/fonts/Nunito-Bold.ttf') format("truetype");
         font-weight: 700;
         font-style: normal;
     }
 
     @font-face {
         font-family: Roboto;
-        src: url('../../public/fonts/Roboto-Medium.eot?') format("eot"),
-        url('../../public/fonts/Roboto-Medium.woff2') format("woff2"),
-        url('../../public/fonts/Roboto-Medium.ttf') format("truetype");
+        src: url('/fonts/Roboto-Medium.eot?') format("eot"),
+        url('/fonts/Roboto-Medium.woff2') format("woff2"),
+        url('/fonts/Roboto-Medium.ttf') format("truetype");
         font-weight: 500;
         font-style: normal;
     }
 
     @font-face {
         font-family: Roboto;
-        src: url('../../public/fonts/Roboto-Bold.eot?') format("eot"),
-        url('../../public/fonts/Roboto-Bold.woff2') format("woff2"),
-        url('../../public/fonts/Roboto-Bold.ttf') format("truetype");
+        src: url('/fonts/Roboto-Bold.eot?') format("eot"),
+        url('/fonts/Roboto-Bold.woff2') format("woff2"),
+        url('/fonts/Roboto-Bold.ttf') format("truetype");
         font-weight: 700;
         font-style: normal;
     }
 
     @font-face {
         font-family: "Playfair Display";
-        src: url('../../public/fonts/PlayfairDisplay-Bold.eot?') format("eot"),
-        url('../../public/fonts/PlayfairDisplay-Bold.woff2') format("woff2"),
-        url('../../public/fonts/PlayfairDisplay-Bold.ttf') format("truetype");
+        src: url('/fonts/PlayfairDisplay-Bold.eot?') format("eot"),
+        url('/fonts/PlayfairDisplay-Bold.woff2') format("woff2"),
+        url('/fonts/PlayfairDisplay-Bold.ttf') format("truetype");
         font-weight: 700;
         font-style: normal;
     }
 
     @font-face {
         font-family: Raleway;
-        src: url('../../public/fonts/Raleway-Medium.eot?') format("eot"),
-        url('../../public/fonts/Raleway-Medium.woff2') format("woff2"),
-        url('../../public/fonts/Raleway-Medium.ttf') format("truetype");
+        src: url('/fonts/Raleway-Medium.eot?') format("eot"),
+        url('/fonts/Raleway-Medium.woff2') format("woff2"),
+        url('/fonts/Raleway-Medium.ttf') format("truetype");
         font-weight: 500;
         font-style: normal;
     }
 
     @font-face {
         font-family: Comfortaa;
-        src: url('../../public/fonts/Comfortaa-Bold.eot?') format("eot"),
-        url('../../public/fonts/Comfortaa-Bold.woff2') format("woff2"),
-        url('../../public/fonts/Comfortaa-Bold.ttf') format("truetype");
+        src: url('/fonts/Comfortaa-Bold.eot?') format("eot"),
+        url('/fonts/Comfortaa-Bold.woff2') format("woff2"),
+        url('/fonts/Comfortaa-Bold.ttf') format("truetype");
         font-weight: 700;
         font-style: normal;
     }
