54754d6de2523e8f4c45f8da80a219ed88a7a4f1
ya_yarek <kvachonoky@gmail.com>
2026-07-15 23:15:04 +0300

    build: remove unused effects and legacy deployment config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
 import {Header} from "./layout/header/Header.tsx";
 import {Main} from "./layout/main/Main.tsx";
 import {Footer} from "./layout/footer/Footer.tsx";
-import {Particle} from "./components/particles/Particles.tsx";
 
 
 function App() {
     return (
         <div className="App">
-            <Particle />
             <Header/>
             <Main/>
             <Footer/>
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
             },
         }),
     ],
-    base: '/portfolio/',
     resolve: {
         alias: {
             "@": "/src",
